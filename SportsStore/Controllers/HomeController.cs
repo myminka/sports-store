e17d57dc28ba1f7af01507fb605cb5ebf6aefe8f
Silkou Maxim <maxim.silkou.2002@gmail.com>
2021-11-23 22:36:08 +0300

    add pages

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using System.Linq;
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
         private IStoreRepository repository;
+        public int PageSize = 4;
 
         public HomeController(IStoreRepository repo)
         {
             this.repository = repo;
         }
-        public IActionResult Index() => View(repository.Products);
+
+        public ViewResult Index(int productPage = 1) =>
+            View(repository.Products
+                .OrderBy(p => p.ProductId)
+                .Skip((productPage - 1) * PageSize)
+            .Take(PageSize));
     }
 }
